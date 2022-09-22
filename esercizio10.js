0d8c9b4a468b73d89cc8b4492eddeed143d9cf8e
Lorenzo_Bavastro <lorenzo.bavastro@gmail.com>
2022-09-22 15:06:41 +0200

    tentativo 1 di fix es 10

--- esercizio10.js
+++ esercizio10.js
@@ -28,8 +28,10 @@ if(Giocatore1 == 0 && Giocatore2 >= 1){
         console.log('la vittoria è del Giocatore 2! Congratulazioni! GG')
         nuovoround = 0;
         
-    } else
-while(nuovoround >= 1 && Giocatore1 > 0 && Giocatore2 > 0){
+        
+    } else if( Giocatore1 > 0 && Giocatore2 > 0 ){
+    
+while(nuovoround >= 1){
             if(Giocatore1 > 0 && Giocatore2 > 0 && mossa1 == 'carta' && mossa2 == 'sasso' ||Giocatore1 > 0 && Giocatore2 > 0 && mossa1 == 'forbice' && mossa2 == 'carta' || mossa1 == 'sasso' && mossa2 == 'forbice'){
                 console.log(mossa1, 'batte...', mossa2,' -1 vita al giocatore 2!')
                 Giocatore2 = Giocatore2 - 1;
@@ -59,16 +61,17 @@ while(nuovoround >= 1 && Giocatore1 > 0 && Giocatore2 > 0){
                         mossa2 = prompt('Inserire la mossa del secondo giocatore: ');
                         console.clear();
                         }
-//                         if(Giocatore1 == 0 && Giocatore2 >= 1){
-//                             console.log('vite giocatore 1: ', Giocatore1, ' e vite del giocatore 2: ', Giocatore2);
-//                             console.log('la vittoria è del Giocatore 2! Congratulazioni! GG')
-//                             nuovoround = 0;
-//                             ;
-//                         } else if(Giocatore2 == 0 && Giocatore1 >= 1){
-//                                     console.log('vite giocatore 1: ', Giocatore1, ' e vite del giocatore 2: ', Giocatore2);
-//                                     console.log('la vittoria è del Giocatore 2! Congratulazioni! GG')
-//                                     nuovoround = 0;
-//                                     break;
+                         if(Giocatore1 == 0 && Giocatore2 >= 1){
+                             console.log('vite giocatore 1: ', Giocatore1, ' e vite del giocatore 2: ', Giocatore2);
+                             console.log('la vittoria è del Giocatore 2! Congratulazioni! GG')
+                             nuovoround = 0;
+                             ;
+                         } else if(Giocatore2 == 0 && Giocatore1 >= 1){
+                                     console.log('vite giocatore 1: ', Giocatore1, ' e vite del giocatore 2: ', Giocatore2);
+                                     console.log('la vittoria è del Giocatore 2! Congratulazioni! GG')
+                                     nuovoround = 0;
+                                     break;
                         
-// }
+ }
 }
+    }
